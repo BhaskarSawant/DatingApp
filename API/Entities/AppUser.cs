addcc83ffe91b220d218b588a1cc5ee03e4b3249
BhaskarSawant <bhaskar.b.sawant@gmail.com>
2021-01-06 20:31:06 -0500

    End of Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,5 +4,9 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+
+        public byte[] PaswordHash { get; set; } 
+
+        public byte[] PasswordSalt { get; set; }    
     }
 }
