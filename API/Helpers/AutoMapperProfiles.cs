ae4756ff41152a003342f0b437191e5638c6e714
BhaskarSawant <bhaskar.b.sawant@gmail.com>
2021-01-20 18:33:24 -0500

    End of section 18 A

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -20,10 +20,10 @@ namespace API.Helpers
             CreateMap<MemberUpdateDto, AppUser>();
             CreateMap<RegisterDto, AppUser>();
             CreateMap<Message, MessageDto>()
-                .ForMember(dest => dest.SenderPhotoUrl, opt => opt.MapFrom(src =>
-                    src.Sender.Photos.First(x => x.IsMain).Url))
+                .ForMember(dest => dest.SenderPhotoUrl, opt => opt.MapFrom(src => 
+                    src.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
                 .ForMember(dest => dest.RecipientPhotoUrl, opt => opt.MapFrom(src =>
-                    src.Recipient.Photos.First(x => x.IsMain).Url));
+                    src.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
             
         }
     }
